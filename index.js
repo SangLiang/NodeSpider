90e6a379b312a83d4d40a601e13a749b33984905
sangliang <378305868@qq.com>
2016-10-24 14:50:35 +0800

    add mysql , make it to be a house-money spider

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 var request = require('request');
+var iconv = require('iconv-lite');
 var cheerio = require('cheerio');
+var mysql = require('mysql');
+// module.exports = {
+//     host: "127.0.0.1",
+//     user: "root",
+//     password: "root",
+//     database: "microblog",
+//     port: "3306"
+// }
 
-var getDate = function(html) {
-	var $ = cheerio.load(html);
-	console.log($('.fcon').length);
-}
-
-request('http://www.aiqitu.cn/',function(error,response,body){
-    if(!error && response.statusCode == 200){
-        console.log(body);
-        getDate(body);
-    }else{
-        console.log(error);
-    }
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "house",
+    port: "3306"
 });
 
+connection.connect();
+var searchUrl = "http://cs.fang.anjuke.com/loupan/yuelu/p1/";
+var id = 0;
+
+function getDate(html) {
+    var $ = cheerio.load(html);
+    var length = $('.items-name').length;
+    var priceLength = $('.price').length;
+    
+    console.log("价格数量" + priceLength);
+    console.log("位置地址" + $('.address').length);
+    for (var i = 0; i < length; i++) {
+        var _placeName = $('.items-name').eq(i).text();
+        var _price = $('.price').eq(i).text();
+        var _position = $('.address').eq(i).find("a").text();
+        ++id;
+        var insertObj = {
+            "id": id,
+            "name": _placeName,
+            "money": _price,
+            "position":_position
+        };
+        writeInSql(insertObj);
+        // console.log($('.house-title').eq(i).text());
+    }
+}
+
+function writeInSql(obj) {
+    // return;
+    connection.query('insert into main set ?', obj, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        console.log('\n');
+    });
+}
+
+request({
+    encoding: null,
+    url: searchUrl
+},
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var buf = iconv.decode(body, 'utf8').toString();
+            getDate(buf);
+            // console.log(buf);
+        } else {
+            console.log(error);
+        }
+    }
+);
